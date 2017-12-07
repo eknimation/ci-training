5a316f01d2f413feef9187a335dace565b198326
akekapon ewit <eknimation@gmail.com>
2017-12-07 19:34:41 +0700

    Load index view

--- application/controllers/Hero.php
+++ application/controllers/Hero.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Hero extends CI_Controller {
 
     public function index() {
-        echo 'Hero Controller';
+        $this->load->view('hero/index');
     }
 
     public function ironman() {
