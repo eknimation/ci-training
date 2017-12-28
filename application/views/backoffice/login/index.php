8acc3e343c017a6d2c5861fe413e5b87ab545b03
akekapon ewit <eknimation@gmail.com>
2017-12-28 15:59:50 +0700

    Create login view

---
+++ application/views/backoffice/login/index.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login form</title>
+    </head>
+    <body>
+        Login form
+    </body>
+</html>
