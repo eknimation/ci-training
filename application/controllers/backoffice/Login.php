aff6ab1967694e0b363c57773b1d0e71237f0f79
akekapon ewit <eknimation@gmail.com>
2018-01-08 16:34:55 +0700

    Create login function

--- application/controllers/backoffice/Login.php
+++ application/controllers/backoffice/Login.php
@@ -9,4 +9,35 @@ class Login extends CI_Controller {
         $this->load->view('backoffice/login/index');
     }
 
+    public function auth() {
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        if (empty($username)) {
+            $http_status = 400;
+            $response = array('message' => 'ระบุ username');
+        } elseif (empty($password)) {
+            $http_status = 400;
+            $response = array('message' => 'ระบุ password');
+        } else {
+            $this->load->model('member_model');
+
+            $response = $this->member_model->read_member_by_username_and_password($username, $password);
+
+            if (!empty($response)) {
+                $http_status = 200;
+                $response = array('message' => 'login successfully');
+            } else {
+                $http_status = 400;
+                $response = array('message' => 'username หรือ password ไม่ถูกต้อง');
+            }
+        }
+
+        $this->output
+                ->set_status_header($http_status)
+                ->set_content_type('application/json', 'utf-8')
+                ->set_output(json_encode($response))
+        ;
+    }
+
 }
--- application/views/backoffice/login/index.php
+++ application/views/backoffice/login/index.php
@@ -34,47 +34,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="login-box">
             <div class="login-logo">
                 <a href="#"></a>
-                   </div>
-                   <!-- /.login-logo -->
-                   <div class="login-box-body">
-                   <p class="login-box-msg">Sign in to start your session</p>
+            </div>
+            <!-- /.login-logo -->
+            <div class="login-box-body">
+                <p class="login-box-msg">Sign in to start your session</p>
 
-                    <form action="" method="post">
-                        <div class="form-group has-feedback">
-                            <input type="email" class="form-control" placeholder="Email">
-                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                        </div>
-                        <div class="form-group has-feedback">
-                            <input type="password" class="form-control" placeholder="Password">
-                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8">
-                                <div class="checkbox icheck">
-                                    <label>
-                                        <input type="checkbox"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-xs-4">
-                                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                    </form>
-
-                    <div class="social-auth-links text-center">
-                        <p>- OR -</p>
-                        <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-                            Facebook</a>
-                        <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-                            Google+</a>
+                <form id="frmLogin">
+                    <div class="form-group has-feedback">
+                        <input name="username" type="text" class="form-control" placeholder="Username">
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                    <!-- /.social-auth-links -->
+                    <div class="form-group has-feedback">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
 
-                    <a href="#">I forgot my password</a><br>
-                    <a href="register.html" class="text-center">Register a new membership</a>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-xs-4">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
 
             </div>
             <!-- /.login-box-body -->
@@ -89,10 +73,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <script src="<?php echo base_url('assets/backoffice/plugins/iCheck/icheck.min.js') ?>"></script>
         <script>
             $(function () {
-                $('input').iCheck({
-                    checkboxClass: 'icheckbox_square-blue',
-                    radioClass: 'iradio_square-blue',
-                    increaseArea: '20%' // optional
+                $("#frmLogin").submit(function (event) {
+
+                    // Stop form from submitting normally
+                    event.preventDefault();
+
+                    $.ajax({
+                        method: "POST",
+                        url: "<?php echo base_url('backoffice/login/auth') ?>",
+                        data: $(this).serialize()
+                    }).done(function (msg) {
+                        alert(msg.message);
+                    }).fail(function (msg) {
+                        var error_msg = msg.responseJSON;
+                        alert(error_msg.message);
+                    });
                 });
             });
         </script>
